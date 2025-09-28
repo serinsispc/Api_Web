b488228705a5ab873876381c2081f560ccd49a0f
emiliano polania <emilianopolania@gmail.com>
2025-09-27 22:54:45 -0500

    27-09-2025    22.54

---
+++ DAL/ModelControl/DBCliente/TokenEmpresaControl.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Models.DBCliente;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ModelControl.DBCliente
+{
+    public class TokenEmpresaControl
+    {
+        public static async Task<TokenEmpresa> Consultar()
+        {
+            try
+            {
+                var cn = new ConnectionSQL();
+                var query = $"select *from tokenEmpresa";
+                var resp=await cn.EjecutarConsulta(query);
+                return JsonConvert.DeserializeObject<TokenEmpresa>(resp);
+            }
+            catch(Exception ex)
+            {
+                string msg = ex.Message;
+                return new TokenEmpresa();
+            }
+        }
+    }
+}
---
+++ DAL/Models/DBCliente/tokenEmpresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models.DBCliente
+{
+    public class TokenEmpresa
+    {
+        public int Id { get; set; }
+        public Guid IdEmpresa { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime FechaCreacion { get; set; }
+    }
+}
