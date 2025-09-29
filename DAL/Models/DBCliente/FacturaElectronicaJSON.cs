c8d7570e9993137faa7933ae9fb2e534686c951c
emiliano polania <emilianopolania@gmail.com>
2025-09-29 18:09:23 -0500

    29-09-2025    18.09

--- DAL/Models/DBCliente/FacturaElectronicaJSON.cs
+++ DAL/Models/DBCliente/FacturaElectronicaJSON.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +11,28 @@ namespace DAL.Models.DBCliente
 {
     public class FacturaElectronicaJSON
     {
-        /// <summary>
-        /// Identificador único del registro
-        /// </summary>
         public int id { get; set; }
-
-        /// <summary>
-        /// Identificador de la venta asociada
-        /// </summary>
         public int idventa { get; set; }
-
-        /// <summary>
-        /// JSON completo de la factura electrónica
-        /// </summary>
-        public string facturaJSON { get; set; }
+        public bool is_valid { get; set; }
+        public bool is_restored { get; set; }
+        public string algorithm { get; set; } = string.Empty;
+        public string zip_key { get; set; } = string.Empty;
+        public string status_code { get; set; } = string.Empty;
+        public string status_description { get; set; } = string.Empty;
+        public string status_message { get; set; } = string.Empty;
+        public string mail_sending_message { get; set; } = string.Empty;
+        public string errors_messages { get; set; } = string.Empty;
+        public string xml_name { get; set; } = string.Empty;
+        public string zip_name { get; set; } = string.Empty;
+        public string signature { get; set; } = string.Empty;
+        public string qr_code { get; set; } = string.Empty;
+        public string qr_link { get; set; } = string.Empty;
+        public string pdf_download_link { get; set; } = string.Empty;
+        public string xml_base64_bytes { get; set; } = string.Empty;
+        public string application_response_base64_bytes { get; set; } = string.Empty;
+        public string attached_document_base64_bytes { get; set; } = string.Empty;
+        public string pdf_base64_bytes { get; set; } = string.Empty;
+        public string zip_base64_bytes { get; set; } = string.Empty;
+        public int type_environment_id { get; set; }
     }
 }
