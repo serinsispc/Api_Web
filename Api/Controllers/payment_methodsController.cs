0b00f254e1f54691d414c2783bbcf2aac2371fc8
emiliano polania <emilianopolania@gmail.com>
2025-10-07 11:09:08 -0500

    7-10-2025   11.08

---
+++ Api/Controllers/payment_methodsController.cs
@@ -0,0 +1,20 @@
+﻿using Api.Class;
+using DAL.ModelControl.DBCliente;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class payment_methodsController : ControllerBase
+    {
+        [HttpPost("Lista_payment")]
+        [TokenAndDb]
+        public async Task<IActionResult> Lista_payment(DBReques reques)
+        {
+            var resp = await payment_methodsControl.Lista_payment();
+            return Ok(resp);
+        }
+    }
+}
---
+++ DAL/ModelControl/DBCliente/payment_methodsControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ModelControl.DBCliente
+{
+    public class payment_methodsControl
+    {
+        public static async Task<RespuestaAPI> Lista_payment()
+        {
+            try
+            {
+                var query = $"SELECT *FROM payment_methods WHERE state=1";
+                var cn = new ConnectionSQL();
+                var resp= await cn.EjecutarConsulta(query,true);
+                if (resp != "[]")
+                {
+                    return new RespuestaAPI { data = resp, estado = true, mensaje = "ok" };
+                }
+                else
+                {
+                    return new RespuestaAPI { mensaje="error", estado=false, data=null };
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                return new RespuestaAPI { data=null, estado=false, mensaje="error" };
+            }
+        }
+    }
+}
