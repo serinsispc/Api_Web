4600ba858d05a2f9a5535fe70f0a58db5e051a36
emiliano polania <emilianopolania@gmail.com>
2025-10-08 11:55:52 -0500

    8-10-2025   11.55

---
+++ Api/Controllers/V_R_MediosDePago_MediosDePagoInternosController.cs
@@ -0,0 +1,20 @@
+﻿using Api.Class;
+using DAL.ModelControl.DBCliente;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class V_R_MediosDePago_MediosDePagoInternosController : ControllerBase
+    {
+        [HttpPost("Lista")]
+        [TokenAndDb]
+        public async Task<IActionResult>Lista(DBReques reques)
+        {
+            var resp = await V_R_MediosDePago_MediosDePagoInternosControl.Lista();
+            return Ok(resp);
+        }
+    }
+}
---
+++ DAL/ModelControl/DBCliente/V_R_MediosDePago_MediosDePagoInternosControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ModelControl.DBCliente
+{
+    public class V_R_MediosDePago_MediosDePagoInternosControl
+    {
+        public static async Task<RespuestaAPI> Lista()
+        {
+            try
+            {
+                var cn = new ConnectionSQL();
+                var query = $"SELECT *FROM V_R_MediosDePago_MediosDePagoInternos";
+                var RESP = await cn.EjecutarConsulta(query,true);
+                if (RESP != "[]")
+                {
+                    return new RespuestaAPI { data=RESP, estado=true, mensaje="ok"};
+                }
+                else
+                {
+                    return new RespuestaAPI { data = null, estado = true, mensaje = "ok" };
+                }
+            }
+            catch(Exception ex)
+            {
+                string error = ex.Message;
+                return new RespuestaAPI { data=null, estado=false, mensaje=error };
+            }
+        }
+    }
+}
